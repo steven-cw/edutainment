eab5fe838bdbbb711945075e63520a827336736b
Steven Williams <stevenw@posteo.net>
2023-10-30 14:50:19 -0700

    Added logic for game

--- edutainment/ContentView.swift
+++ edutainment/ContentView.swift
@@ -11,6 +11,12 @@ struct ContentView: View {
     @State private var gameActive = false
     @State private var timesTable = 2
     @State private var questionsSelection = 5
+    @State private var num1 = 0
+    @State private var num2 = 0
+    @State private var correctAnswer = 0
+    @State private var input = 0
+    @State private var correct = 0
+    @State private var table = [Int]()
     
     var questions = [5, 10, 20]
     
@@ -35,14 +41,46 @@ struct ContentView: View {
                     }
             
             Section {
-                
+                Text("What is \(num1) x \(num2)")
+            }
+            
+            Section {
+                TextField("Amount", value: $input, format:
+                        .number)
+                .keyboardType(.decimalPad)
+            } header: {
+                Text("Enter number:")
+            }
+            
+            Button {
+                checkAnswer()
+                newQuestion()
+            }
+                    label: {
+                        Text("Submit Answer")
+                    }
+            Section {
+                Text("Correct: \(correct)")
             }
         }
         .padding()
     }
     
     func startGame() {
-        var table1 = [Int](0...questionsSelection)
+        table = [Int](0...questionsSelection)
+        newQuestion()
+    }
+    
+    func newQuestion() {
+        num1 = table.randomElement() ?? 0
+        num2 = table.randomElement() ?? 0
+        correctAnswer = num1 * num2
+    }
+    
+    func checkAnswer() {
+        if input == correctAnswer {
+            correct += 1
+        }
     }
 }
 
